599fa5a1bd7fdf573fde1b5cfe349ef1cdb423d0
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-01 18:22:35 -0300

    feat: list functionality

--- src/module-app.ts
+++ src/module-app.ts
@@ -1,8 +1,48 @@
-import { QinLabel } from "qin_case";
+import { QinButton, QinColumn, Qine, QinLabel, QinPanel, QinTabs, QinText } from "qin_case";
 import { Module } from "./module";
+import { QinEvent } from "../../qin_soul/types/qin-arms";
 
 export class ModuleAPP extends Module {
+    private _bodyTabs = new QinTabs();
+    private _listPanel = new ListPanel();
+    private _manifestPanel = new ManifestPanel();
+    private _getPanel = new GetPanel();
+    
     public constructor() {
-        super("APP", new QinLabel("TESTE"));
+        super();
+        this._bodyTabs
+            .addTab({title: "List", viewer: this._listPanel})
+            .addTab({title: "Manifest", viewer: this._manifestPanel})
+            .addTab({title: "Get", viewer: this._getPanel})
+            .install(this);
+    }
+}
+
+class ListPanel extends QinColumn {
+    private _listButton = new QinButton({label: new QinLabel("List")});
+    private _resultText = new QinText();
+
+    public constructor() {
+        super();
+        this._listButton.addActionMain(_ => this.actList()).install(this);
+        this._resultText.install(this);
+    }
+
+    private actList() {
+        this.qinpel.talk.app.list()
+                .then((res) => this._resultText.value = res.join("\n"))
+                .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000001)"))
+    }
+}
+
+class ManifestPanel extends QinPanel {
+    public constructor() {
+        super();
+    }
+}
+
+class GetPanel extends QinPanel {
+    public constructor() {
+        super();
     }
 }
