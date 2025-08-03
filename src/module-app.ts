cc249a3429810cee5769e2f09f3362e19159a073
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-03 10:23:17 -0300

    feat: app functionality

--- src/module-app.ts
+++ src/module-app.ts
@@ -1,12 +1,11 @@
-import { QinButton, QinColumn, Qine, QinLabel, QinPanel, QinTabs, QinText } from "qin_case";
+import { QinButton, QinColumn, QinLabel, QinLine, QinString, QinTabs, QinText, QinTitled } from "qin_case";
 import { Module } from "./module";
-import { QinEvent } from "../../qin_soul/types/qin-arms";
 
 export class ModuleAPP extends Module {
     private _bodyTabs = new QinTabs();
     private _listPanel = new ListPanel();
     private _manifestPanel = new ManifestPanel();
-    private _getPanel = new GetPanel();
+    private _assetPanel = new AssetPanel();
     
     public constructor() {
         super();
@@ -14,12 +13,13 @@ export class ModuleAPP extends Module {
         this._bodyTabs
             .addTab({title: "List", viewer: this._listPanel})
             .addTab({title: "Manifest", viewer: this._manifestPanel})
-            .addTab({title: "Get", viewer: this._getPanel})
+            .addTab({title: "Asset", viewer: this._assetPanel})
             .install(this);
     }
 }
 
 class ListPanel extends QinColumn {
+    private _actionLine = new QinLine();
     private _listButton = new QinButton({label: new QinLabel("List")});
     private _resultText = new QinText();
 
@@ -27,25 +27,68 @@ class ListPanel extends QinColumn {
         super();
         this.styleAsWhole();
         this._resultText.styleAsWhole();
-        this._listButton.addActionMain(_ => this.actList()).install(this);
+        this._listButton.addActionMain(_ => this.actList());
+        this._listButton.install(this._actionLine);
+        this._actionLine.install(this);
         this._resultText.install(this);
     }
 
     private actList() {
         this.qinpel.talk.app.list()
-                .then((res) => this._resultText.value = res.join("\n"))
-                .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000001)"))
+            .then((res) => this._resultText.value = res.join("\n"))
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000001)"))
     }
 }
 
-class ManifestPanel extends QinPanel {
+class ManifestPanel extends QinColumn {
+    private _actionLine = new QinLine();
+    private _nameString = new QinString();
+    private _nameTitled = new QinTitled({title: "Name", items: [this._nameString]});
+    private _manifestButton = new QinButton({label: new QinLabel("Manifest")});
+    private _resultText = new QinText();
+
     public constructor() {
         super();
+        this.styleAsWhole();
+        this._resultText.styleAsWhole();
+        this._manifestButton.addActionMain(_ => this.actManifest());
+        this._nameTitled.install(this._actionLine);
+        this._manifestButton.install(this._actionLine);
+        this._actionLine.install(this);
+        this._resultText.install(this);
+    }
+
+    private actManifest() {
+        this.qinpel.talk.app.manifest(this._nameString.value)
+            .then((res) => this._resultText.value = JSON.stringify(res))
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000002)"))
     }
 }
 
-class GetPanel extends QinPanel {
+class AssetPanel extends QinColumn {
+    private _nameString = new QinString();
+    private _nameTitled = new QinTitled({title: "Name", items: [this._nameString]});
+    private _assetString = new QinString();
+    private _assetTitled = new QinTitled({title: "Asset", items: [this._assetString]});
+    private _assetButton = new QinButton({label: new QinLabel("Asset")});
+    private _actionLine = new QinLine();
+    private _resultText = new QinText();
+
     public constructor() {
         super();
+        this.styleAsWhole();
+        this._resultText.styleAsWhole();
+        this._assetButton.addActionMain(_ => this.actAsset());
+        this._nameTitled.install(this._actionLine);
+        this._assetTitled.install(this._actionLine);
+        this._assetButton.install(this._actionLine);
+        this._actionLine.install(this);
+        this._resultText.install(this);
+    }
+
+    private actAsset() {
+        this.qinpel.talk.app.asset<string>(this._nameString.value, this._assetString.value)
+            .then((res) => this._resultText.value = res)
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000002)"))
     }
 }
