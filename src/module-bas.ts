ebd12fd1e20d164ce0753a5915e980efdcfbc955
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-05 11:03:37 -0300

    feat: bas functionality

--- src/module-bas.ts
+++ src/module-bas.ts
@@ -1,9 +1,27 @@
-import { QinLabel } from "qin_case";
+import { QinButton, QinColumn, QinLabel, QinLine, QinString, QinTabs, QinText, QinTitled } from "qin_case";
 import { Module } from "./module";
 
 export class ModuleBAS extends Module {
+    private _bodyColumn = new QinColumn();
+    private _actionLine = new QinLine();
+    private _listButton = new QinButton({label: new QinLabel("List")});
+    private _resultText = new QinText();
+
     public constructor() {
         super();
-        new QinLabel("BAS").install(this);
+        this.styleAsWhole();
+        this._bodyColumn.styleAsWhole();
+        this._resultText.styleAsWhole();
+        this._listButton.addActionMain(_ => this.actList());
+        this._listButton.install(this._actionLine);
+        this._actionLine.install(this._bodyColumn);
+        this._resultText.install(this._bodyColumn);
+        this._bodyColumn.install(this);
+    }
+
+    private actList() {
+        this.qinpel.talk.bas.list()
+            .then((res) => this._resultText.value = res.join("\n"))
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000001)"))
     }
 }
