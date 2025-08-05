33c772c26ae3e3c680a451d69393255510e3a7f7
Éverton M. Vieira <emuvi@outlook.com.br>
2025-08-05 14:50:45 -0300

    feat: cmd functionality

--- src/module-cmd.ts
+++ src/module-cmd.ts
@@ -1,9 +1,87 @@
-import { QinLabel } from "qin_case";
+import { QinBoolean, QinButton, QinColumn, QinCombo, QinLabel, QinLine, QinString, QinTabs, QinText, QinTitled } from "qin_case";
 import { Module } from "./module";
+import { Execute } from "../../qin_soul/types/qin-type";
 
 export class ModuleCMD extends Module {
+    private _bodyTabs = new QinTabs();
+    private _listPanel = new ListPanel();
+    private _runPanel = new RunPanel();
+
     public constructor() {
         super();
-        new QinLabel("CMD").install(this);
+        this._bodyTabs.styleAsWhole();
+        this._bodyTabs.addTab({title: "List", viewer: this._listPanel});
+        this._bodyTabs.addTab({title: "Run", viewer: this._runPanel});
+        this._bodyTabs.install(this);
+    }
+}
+
+class ListPanel extends QinColumn {
+    private _actionLine = new QinLine();
+    private _listButton = new QinButton({label: new QinLabel("List")});
+    private _resultText = new QinText();
+
+    public constructor() {
+        super();
+        this.styleAsWhole();
+        this._resultText.styleAsWhole();
+        this._listButton.addActionMain(_ => this.actList());
+        this._listButton.install(this._actionLine);
+        this._actionLine.install(this);
+        this._resultText.install(this);
+    }
+
+    private actList() {
+        this.qinpel.talk.cmd.list()
+            .then((res) => this._resultText.value = res.join("\n"))
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000003)"))
+    }
+}
+
+class RunPanel extends QinColumn {
+    private _actionLine = new QinLine();
+    private _nameString = new QinString();
+    private _nameTitled = new QinTitled({label: new QinLabel("Name"), items: [this._nameString]});
+    private _argsText = new QinText();
+    private _argsTitled = new QinTitled({label: new QinLabel("Args"), items: [this._argsText]});
+    private _inputText = new QinText();
+    private _inputTitled = new QinTitled({label: new QinLabel("Input"), items: [this._inputText]});
+    private _joinErrsBoolean = new QinBoolean();
+    private _joinErrsTitled = new QinTitled({label: new QinLabel("Join Errors"), items: [this._joinErrsBoolean]});
+    private _logLevelCombo = new QinCombo({items: [{title: "Error", value: "0"}, {title: "Warn", value: "1"}, {title: "Info", value: "2"}, {title: "Debug", value: "3"}, {title: "Trace", value: "4"}]});
+    private _logLevelTitled = new QinTitled({label: new QinLabel("Log Level"), items: [this._logLevelCombo]});
+    private _runButton = new QinButton({label: new QinLabel("Run")});
+    private _resultText = new QinText();
+
+    public constructor() {
+        super();
+        this.styleAsWhole();
+        this._resultText.styleAsWhole();
+        this._nameTitled.install(this._actionLine);
+        this._argsTitled.install(this._actionLine);
+        this._inputTitled.install(this._actionLine);
+        this._joinErrsTitled.install(this._actionLine);
+        this._logLevelTitled.install(this._actionLine);
+        this._runButton.addActionMain(_ => this.actRun());
+        this._runButton.install(this._actionLine);
+        this._actionLine.install(this);
+        this._resultText.install(this);
+    }
+
+    private getExecute(): Execute {
+        return {
+            name: this._nameString.value,
+            args: this._argsText.getLines(),
+            input: this._inputText.getLines(),
+            joinErrs: this._joinErrsBoolean.value,
+            logLevel: parseInt(this._logLevelCombo.value, 10)
+        };
+    }
+
+    private actRun() {
+        const execute = this.getExecute();
+        this.qinpel.talk.cmd.run(execute)
+            .then((token) => this._resultText.appendLine(token + "\n" + JSON.stringify(execute)))
+            .catch((err) => this.qinpel.frame.statusError(err, "{qia_abdesk}(ErrCode-000003)"))
     }
 }
