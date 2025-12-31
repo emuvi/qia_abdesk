80a44692ca0d4eebd307eb4d3fdbf03fb4a30bca
Éverton M. Vieira <emuvi@outlook.com.br>
2025-12-31 11:04:20 -0300

    feat: test splitter

--- src/module-tst.ts
+++ src/module-tst.ts
@@ -1,12 +1,20 @@
 import { QinSplitter } from "qin_case";
 import { Module } from "./module";
+import { ModuleWAY } from "./module-way";
+import { ModuleUTL } from "./module-utl";
 
 export class ModuleTST extends Module {
 
     private _testSplitter = new QinSplitter();
 
+    private _moduleWAY = new ModuleWAY();
+    private _moduleUTL = new ModuleUTL();
+
     public constructor() {
         super();
+        this._testSplitter.setSideA(this._moduleWAY);
+        this._testSplitter.setSideB(this._moduleUTL);
+        this._testSplitter.styleAsWhole();
         this._testSplitter.install(this);
     }
 
